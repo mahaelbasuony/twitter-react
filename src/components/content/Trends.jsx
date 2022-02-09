affa36e935882dd82761e2ca00d26e17126ab4d8
Maha Elbasuony <mahaelbasuony@gmail.com>
2022-02-09 22:01:06 +0100

    trends

--- src/components/content/Trends.jsx
+++ src/components/content/Trends.jsx
@@ -1,1 +1,92 @@
+import Icon from "../Icon";
 import "./trends.css";
+
+export const Trends = () => {
+  return (
+    <div class="right-col__trends">
+      <header class="trends__header">
+        <h3 class="trends__header__title">Trends for you </h3>
+        <a href="#" class="trends__header__settings" title="settings">
+          <Icon
+            class="trends__container__more-info__icon"
+            name={"settings"}
+            color={"#fff"}
+            size={20}
+          />
+        </a>
+      </header>
+      <ul>
+        <li class="trends__container">
+          <span class="trends__container__hashtag">Trending In Nederlands</span>
+          <a href="#" class="trends__container__more-info" title="more">
+            <Icon
+              class="trends__container__more-info__icon"
+              name={"more"}
+              color={"#fff"}
+              size={20}
+            />
+          </a>
+          <h4 class="trends__container__title">Bergkamp</h4>
+          <span class="trends__container__number-of-tweets">1,391 tweets</span>
+        </li>
+        <li class="trends__container">
+          <span class="trends__container__hashtag">Trending In Nederlands</span>
+          <a href="#" class="trends__container__more-info" title="more">
+            <Icon
+              class="trends__container__more-info__icon"
+              name={"more"}
+              color={"#fff"}
+              size={20}
+            />
+          </a>
+          <h4 class="trends__container__title">viruswaarheid</h4>
+          <span class="trends__container__number-of-tweets">1,782 tweets</span>
+        </li>
+        <li class="trends__container">
+          <span class="trends__container__hashtag">Trending In Nederlands</span>
+          <a href="#" class="trends__container__more-info" title="more">
+            <Icon
+              class="trends__container__more-info__icon"
+              name={"more"}
+              color={"#fff"}
+              size={20}
+            />
+          </a>
+          <h4 class="trends__container__title">#kutrabobank</h4>
+          <span class="trends__container__number-of-tweets">2,029 tweets</span>
+        </li>
+        <li class="trends__container">
+          <span class="trends__container__hashtag">Policis . Trending</span>
+          <a href="#" class="trends__container__more-info" title="more">
+            <Icon
+              class="trends__container__more-info__icon"
+              name={"more"}
+              color={"#fff"}
+              size={20}
+            />
+          </a>
+          <h4 class="trends__container__title">Kaag</h4>
+          <span class="trends__container__number-of-tweets">19K tweets</span>
+        </li>
+        <li class="trends__container">
+          <span class="trends__container__hashtag">Auto racing . Trending</span>
+          <a href="#" class="trends__container__more-info" title="more">
+            <Icon
+              class="trends__container__more-info__icon"
+              name={"more"}
+              color={"#fff"}
+              size={20}
+            />
+          </a>
+          <h4 class="trends__container__title">#DutchGP</h4>
+          <span class="trends__container__number-of-tweets"></span>
+        </li>
+      </ul>
+      <footer class="trends__footer">
+        <a href="#" class="trends__footer__show-more">
+          Show more
+        </a>
+      </footer>
+    </div>
+  );
+};
--- src/components/right-side-components/RightSide.jsx
+++ src/components/right-side-components/RightSide.jsx
@@ -1,10 +1,12 @@
 import "./rightSide.css";
 import { Header } from "./Header";
+import { Trends } from "../content/Trends";
 
 export const RightSide = () => {
   return (
     <div className="right-col">
       <Header />
+      <Trends />
     </div>
   );
 };
